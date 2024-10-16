b2bbff2ff82a011061d06de6f2a922331a31527f
Damian-Vera <DamianVera28122000@proton.me>
2024-10-16 17:35:01 -0300

    Update Pokedex.sql

--- SQL/Pokedex.sql
+++ SQL/Pokedex.sql
@@ -14,5 +14,14 @@ Create Table Pokemon (
 	PRIMARY KEY(id)
 );
 
+-- jugando un poco con el select
+SELECT * FROM pokemon WHERE tipo = 'psíquico' AND tipo_Secundario = 'hada';
+SELECT * FROM POKEMON WHERE tipo = 'fuego' AND pesoKG > 100;
+SELECT * FROM pokemon WHERE nombre_preevo IS NOT NULL;
+SELECT * FROM pokemon WHERE tipo IN ('agua') and tipo_secundario IS NOT NULL;
+SELECT * FROM pokemon WHERE TIPO NOT IN ('normal', 'veneno', 'fantasma' , 'bicho') 
+AND tipo_secundario NOT IN ('normal', 'veneno', 'fantasma' , 'bicho');
+-- olvide quitar las tildes. :P
+
 
 
